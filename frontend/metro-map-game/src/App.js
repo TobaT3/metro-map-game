f21468dcf78e0675a86d452ecfa83b702f508d0e
BrokenFry <119271344+BrokenFry@users.noreply.github.com>
2023-05-15 15:40:22 +0200

    Style change
    Changed css file to make it look a lil bit nicer
    App now has placeholder text

--- frontend/metro-map-game/src/App.js
+++ frontend/metro-map-game/src/App.js
@@ -4,19 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header id="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sup dude
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>BEGIN</button>
       </header>
     </div>
   );
