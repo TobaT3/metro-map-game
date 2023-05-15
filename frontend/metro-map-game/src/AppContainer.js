035cf4e73dc1f5d912bd3a179f5007db3361b59f
BrokenFry <119271344+BrokenFry@users.noreply.github.com>
2023-05-15 16:56:51 +0200

    Form layout finished (for now)
    Changed bg to improve text visibility
    Added text field and submit button
    Made container keep track of input text
    Form can accept a submit function (will do soon)

--- frontend/metro-map-game/src/AppContainer.js
+++ frontend/metro-map-game/src/AppContainer.js
@@ -1,11 +1,13 @@
 import App from './App';
 import Form from './form';
+import { useState } from 'react';
 
 function AppContainer(){
+    const [inputValue, setInputValue] = useState('');
     return(
         //<App />
         //TODO: make it render app first, then switch to form
-        <Form />
+        <Form fieldValue={inputValue} valueChanger={setInputValue}/>
     )
 }
 
--- frontend/metro-map-game/src/form.js
+++ frontend/metro-map-game/src/form.js
@@ -1,11 +1,15 @@
 import './App.css';
 
-function Form({ inputValue }) {
+function Form({ fieldValue, valueChanger, submitter }) {
   return (
     <div className="Form">
       <div className='content'>
         <p>Guess the thing!</p>
         <img src = {require("./images/placeholder.png")}/>
+        <form onSubmit={submitter}>
+            <input type='text' value = {fieldValue} onChange={(e) => {valueChanger(e.target.value)}}></input>
+            <input type = 'submit'></input>
+        </form>
       </div>
     </div>
   );
