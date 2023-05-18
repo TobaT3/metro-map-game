5050dec9da1ac7cadd6e8ea6cd15d44f3a56d838
BrokenFry <119271344+BrokenFry@users.noreply.github.com>
2023-05-18 17:05:21 +0200

    Score now persists between renders
    finally

--- frontend/metro-map-game/src/AppContainer.js
+++ frontend/metro-map-game/src/AppContainer.js
@@ -1,21 +1,27 @@
 import App from './App';
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, redirect } from "react-router-dom";
 import Form from './form';
-import { useState } from 'react';
+import useLocalStorageState from 'use-local-storage-state';
+import { useState, useEffect } from 'react';
 
 function AppContainer(){
     const [inputValue, setInputValue] = useState('');
-    const [score, setScore] = useState(0);
+    const [score, setScore] = useLocalStorageState('score', 0);
     const [correctAnswer, setCorrectAnswer] = useState('Amogus');
+    const submitHandler = () => {
+        if(inputValue === correctAnswer){
+            setScore(score + 1);
+        }else{
+            setScore(0);
+        }
+    }
 
-
-    
     return(
         <>
         <BrowserRouter>
             <Routes>
                 <Route index element={<App />}></Route>
-                <Route path='game' element={<Form fieldValue={inputValue} valueChanger={setInputValue} score = {score}/>}></Route>
+                <Route path='game' element={<Form fieldValue={inputValue} valueChanger={setInputValue} score = {score} submitter={submitHandler}/>}></Route>
             </Routes>
         </BrowserRouter>
         <Outlet />
